5792434e9f1789a97787c8b21b01f819bc6964c0
matiaslijtenstein <71510749+matiaslijtenstein@users.noreply.github.com>
2022-05-20 18:14:04 -0300

    Agrego nuevo proyecto de Specflow

---
+++ Obligatorio/Ministerio de Turismo/SpecFlowProject.Specs/Features/Calculator.feature
@@ -0,0 +1,78 @@
+﻿Feature: Calculator
+![Calculator](https://specflow.org/wp-content/uploads/2020/09/calculator.png)
+Simple calculator for adding **two** numbers
+
+Link to a feature: [Calculator](SpecFlowProject.Specs/Features/Calculator.feature)
+***Further read***: **[Learn more about how to generate Living Documentation](https://docs.specflow.org/projects/specflow-livingdoc/en/latest/LivingDocGenerator/Generating-Documentation.html)**
+
+@mytag
+Scenario: Add correct point of charge
+	Given the identificator is 1234
+	And the name is Electric Charge
+	And the address is Rambla Wilson
+	And the region is Este
+	And the description is Full charge quickly
+	When the create button is pressed
+	Then the result should be Point of charge added
+
+Scenario: Non numeric ID
+	Given the identificator is ABCD
+	And the name is Electric Charge
+	And the address is Rambla Wilson
+	And the region is Este
+	And the description is Full charge quickly
+	When the create button is pressed
+	Then the result should be The id must be numeric
+
+Scenario: Non existent region
+	Given the identificator is 1234
+	And the name is Electric Charge
+	And the address is Rambla Wilson
+	And the region is Litoral Oeste
+	And the description is Full charge quickly
+	When the create button is pressed
+	Then the result should be Inexistent region
+
+Scenario: Name greater than 20 characters
+	Given the identificator is 1234
+	And the name is High power electric charger 
+	And the address is Rambla Wilson
+	And the region is Este
+	And the description is Full charge quickly
+	When the create button is pressed
+	Then the result should be Name must be shorter than 20 characters
+
+Scenario: Description greater than 60 characters
+	Given the identificator is 1234
+	And the name is Electric Charge
+	And the address is Rambla Wilson
+	And the region is Este
+	And the description is This high power electric charger is highly recommended. It will charge your car in just 1 hour and 30 minutes. It is also located in a region worth visiting
+	When the create button is pressed
+	Then the result should be Description must be shorter than 60 characters
+
+Scenario: Address greater than 30 characters
+	Given the identificator is 1234
+	And the name is Electric Charge
+	And the address is Advenir at University Park Apartments, 10495 SW 14th Ter, Miami, FL 33174
+	And the region is Este
+	And the description is Full charge quickly
+	When the create button is pressed
+	Then the result should be Address must be shorter than 30 characters
+
+Scenario: Null field
+	Given the identificator is 1234
+	And the name is Electric Charge
+	And the region is Este
+	And the description is Full charge quickly
+	When the create button is pressed
+	Then the result should be Every field is mandatory
+
+Scenario: Repeated ID
+	Given the identificator is 1234
+	And the name is Electric Charge
+	And the address is Rambla Wilson
+	And the region is Este
+	And the description is Full charge quickly
+	When the create button is pressed
+	Then the result should be The ID already exists
---
+++ Obligatorio/Ministerio de Turismo/SpecFlowProject.Specs/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,92 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SpecFlowProject.Specs.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the identificator is (.*)")]
+        public void GivenTheIdIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            _scenarioContext.Pending();
+        }
+
+        [Given("the name is (.*)")]
+        public void GivenTheNameIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            _scenarioContext.Pending();
+        }
+
+        [Given("the address is (.*)")]
+        public void GivenTheAddressIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            _scenarioContext.Pending();
+        }
+        [Given("the region is (.*)")]
+        public void GivenTheRegionIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            _scenarioContext.Pending();
+        }
+        [Given("the description is (.*)")]
+        public void GivenTheDescriptionIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            _scenarioContext.Pending();
+        }
+
+        [When("the create button is pressed")]
+        public void WhenTheCreateButtonIsPressed()
+        {
+            //TODO: implement act (action) logic
+
+            _scenarioContext.Pending();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            //TODO: implement assert (verification) logic
+
+            _scenarioContext.Pending();
+        }
+    }
+}
